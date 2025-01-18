98132b286e77d0716167b3eafffdb99ed8f80862
Márcio Miguel <marciorodrigo2@hotmail.com>
2025-01-18 17:22:54 +0000

    Add custom admin styles and templates for enhanced dashboard appearance

--- backend/settings.py
+++ backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Moved this line here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]

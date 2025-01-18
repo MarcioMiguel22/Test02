d8dc185a760a07c5e4e00d52cfc40cfc6c109b2c
Márcio Miguel <marciorodrigo2@hotmail.com>
2025-01-18 17:10:21 +0000

    Refactor URL routing and enhance home page template with improved design and content

--- nome_do_app/urls.py
+++ nome_do_app/urls.py
@@ -1,11 +1,6 @@
-from xml.etree.ElementInclude import XINCLUDE
-from django.http import HttpResponse
-from django.urls import include, path
+from django.urls import path
 from . import views
-from nome_do_app import admin
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('nome_do_app.urls')),  
+    path('', views.home, name='home'),
 ]

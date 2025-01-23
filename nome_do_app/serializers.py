658e8c8d69e00f437885f2c4961b28ac10b8bbb0
Márcio Miguel <marciorodrigo2@hotmail.com>
2025-01-23 14:31:20 +0000

    Add custom representation logic to CodigoEntradaSerializer to handle invalid values for 'codigos_da_porta'

--- nome_do_app/serializers.py
+++ nome_do_app/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import CodigoEntrada
 
 class CodigoEntradaSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Corrigir valores inválidos
+        data['codigos_da_porta'] = (
+            [] if data['codigos_da_porta'] in [None, 'nan'] else data['codigos_da_porta'].split(',')
+        )
+        return data
+
     class Meta:
+        model = CodigoEntrada
         model = CodigoEntrada
         fields = '__all__'

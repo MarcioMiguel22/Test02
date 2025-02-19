d18db345559f8ca2ff1d1388bca834238995db4b
Márcio Miguel <marciorodrigo2@hotmail.com>
2025-02-19 01:06:35 +0000

    Implement bulk data saving functionality and enhance Avaria creation with logging

--- avarias_a_receber/serializers.py
+++ avarias_a_receber/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Avaria, Material  # Corrected import
+from .models import Avaria, Material
 
 class AvariaSerializer(serializers.ModelSerializer):
     class Meta:
